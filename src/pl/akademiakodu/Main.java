cc56e6fddc126006c319f5551b2c13ee7bcac0a2
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-05-23 20:58:50 +0200

    Initial commit

---
+++ src/pl/akademiakodu/Main.java
@@ -0,0 +1,91 @@
+package pl.akademiakodu;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> myList = Arrays.asList("Oskar", "Piotr", "Kondzio", "Kuba");
+
+//        FILTROWANIE INFORMACJI:
+        myList.stream()
+                .filter(s -> s.startsWith("O"))
+                .forEach(s -> System.out.println(s));
+
+//        SORTOWANIE INFORMACJI:
+        myList.stream()
+                .filter(s -> s.length() > 4)                    //warunek ze imie musi byc wieksze niz 4 znaki
+                .sorted((s, s1) -> s.compareToIgnoreCase(s1))   //sortowanie
+                .map(s -> s.toUpperCase())                      //podbijanie do wielkich znakow
+//                .forEach(s -> System.out.println(s));
+//                             LUB
+                .findFirst()
+                .ifPresent(s -> System.out.println(s));
+
+//        OPTIONAL SPRAWDZAJACY/ZABEZPIECZAJACY PRZED NULL-ami
+//        Optional<String> optional = Optional.of(null);
+
+//        if(optional.isPresent()){
+//            System.out.println(optional.get());
+//        }else{
+//            System.out.println("Ten obiekt jest nullem");
+//        }
+//                             LUB
+//        optional.ifPresent(s -> System.out.println(s));
+
+        System.out.println("-----------------------------------------------------------------------------------");
+//        INNY SPOSOB TWORZENIA STREAMA
+
+        Stream.of("a142", "a2124", "a323")
+                .map(s -> s.substring(1))
+                .mapToInt(s -> Integer.valueOf(s))
+//                .average()
+                .map(s -> s + 100)
+                .min()
+                .ifPresent(s -> System.out.println(s));
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        IntStream.of(5, 10, 124, 123)
+                .average()
+//                .ifPresent(System.out::println); //COLON - stricte do lambd
+//                          LUB
+                .ifPresent(s -> System.out.println(s));
+
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Oskar", 26));
+        personList.add(new Person("Piotr", 20));
+        personList.add(new Person("Lukasz", 70));
+        personList.add(new Person("Kuba", 14));
+
+        String message = personList.stream()
+                .filter(person -> person.getAge() >= 18)
+                .map(person -> person.getName())
+                .collect(Collectors.joining(" i ", "Powyzej 18 lat sa: ", ". "));
+
+        System.out.println(message);
+
+        Double aver = personList.stream()
+                .collect(Collectors.averagingInt(person -> person.getAge()));
+        System.out.println("Srednia wieku to: " + aver);
+
+        IntSummaryStatistics statistics = personList.stream()
+                .collect(Collectors.summarizingInt(person -> person.getAge()));
+        System.out.println("Statystyki: " + statistics);
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        List<String> converted = personList.stream()
+                .map(s -> s.getName())
+                .collect(Collectors.toList());
+        System.out.println(converted);
+    }
+}
---
+++ src/pl/akademiakodu/Person.java
@@ -0,0 +1,51 @@
+package pl.akademiakodu;
+
+/**
+ * Created by Lukasz Kolacz on 23.05.2017.
+ */
+public class Person {
+
+    private String name;
+    private String lastname;
+    private int age;
+    private char sex;
+
+
+    public Person(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+}
